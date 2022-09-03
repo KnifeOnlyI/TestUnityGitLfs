766f1a672e93e8221c4b1979456231442919b943
Dany Pignoux <dany.pignoux@outlook.fr>
2022-09-03 14:40:15 +0200

    Update the cube

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Hello World !");
     }
 }
